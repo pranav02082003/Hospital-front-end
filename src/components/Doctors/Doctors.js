3badbd3d63bd509e5b5b340e70c6c0881d86d7d2
pranav02082003 <luckypranav47@gmail.com>
2023-09-13 20:20:14 +0530

    doctors

--- src/components/Doctors/Doctors.js
+++ src/components/Doctors/Doctors.js
@@ -1,7 +1,30 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
+import axios from 'axios'
 
 export default function Doctors() {
+
+  const [doctors,setDoctors]  =useState([])
+
+  const fetchData = () => {
+    axios.get('http://localhost:4000/data').then((res) => {
+      console.log(res.data)
+      setDoctors(res.data)
+    }).catch((err) => console.log(err))
+  }
+  
+  useEffect(() => {
+    fetchData()
+  },[])
+
   return (
-    <div>Doctors</div>
+    <div>
+      {doctors.map((each)=> {
+        return <div>
+          <img src={each.image} alt={each.name}/>
+          <p>{each.name}</p>
+          <span>{each.Designation}</span>
+        </div>
+      })}
+    </div>
   )
 }
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -9,7 +9,8 @@ export default function Home() {
             <span className='medical'>Medical</span>
             <span className='services'>Services</span>
         </div>
-        <h1>Hospital Management</h1>
+        <div className='heading'>Hospital Management</div>
+        <b>To check the doctors availabilty and book <br></br> an appointment click below</b>
         <Link to={'/doctors'}><button id='hello' className='home-button'>View All Doctors</button></Link>
         
     </div>
