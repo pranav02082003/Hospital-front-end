47660efb3c0279095e70e68507f2a2904bf2b7d1
pranav02082003 <luckypranav47@gmail.com>
2023-09-16 21:47:43 +0530

    validation

--- src/components/Doctor/Doctor.js
+++ src/components/Doctor/Doctor.js
@@ -9,9 +9,13 @@ export default function Doctor() {
 
   const [data, setData] = useState([])
   const [timings, setTimings] = useState([])
+  const [name, setName] = useState("")
+  const [age, setAge] = useState('')
+  const [problem, setProblem] = useState('')
+  const [mobile, setMobile] = useState('')
+  const [error, setError] = useState('')
 
   const { id } = useParams()
-  console.log(id)
 
   const fetchData = () => {
     axios.get('http://localhost:4000/data').then((res) => {
@@ -23,18 +27,41 @@ export default function Doctor() {
 
     }).catch((err) => console.log(err))
   }
-  const handleModal = () => {
+
+  const handleMobile = (e) => {
+    setMobile(e.target.value)
+    if (e.target.value.length > 10 || e.target.value.length < 10) {
+      console.log("entter valid")
+      setError("Enter valid number")
+    } else {
+      setError("")
+    }
+  }
+  // console.log("mobile: " + mobile)
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (mobile.length > 10 || mobile.length < 10) {
+      alert("enter valid mobile")
+    }
+  }
+
+  const handleModal = (time) => {
     Modal.success({
       content: <div>
-        <form>
+        <form className='d-flex flex-column gap-3' onSubmit={handleSubmit}>
+          <label>Time</label>
+          <input value={time} disabled className='form-control'/>
           <TextField type='text' id="standard-basic" label="Patient Name" variant="standard" />
           <TextField type='number' id="standard-basic" label="Age" variant="standard" />
           <TextField type='text' id="standard-basic" label="Problem" variant="standard" />
+          <TextField type='tel' onChange={handleMobile} id="standard-basic" label="Mobile Number" variant="standard" />
+          <button type="Submit" className="btn btn-success">Submit</button>
         </form>
       </div>,
       centered: true,
       maskClosable: true,
-      footer:null
+      footer: null
     })
   }
 
@@ -54,7 +81,7 @@ export default function Doctor() {
       <div className='d-flex gap-3 button-group'>
         {timings.map((eachtime) => {
           return <div key={eachtime.id}>
-            {eachtime.status ? <button onClick={handleModal}>{eachtime.time}</button> : <button disabled>{eachtime.time}</button>}
+            {eachtime.status ? <button onClick={() => handleModal(eachtime.time)}>{eachtime.time}</button> : <button disabled>{eachtime.time}</button>}
           </div>
         })}
       </div>
