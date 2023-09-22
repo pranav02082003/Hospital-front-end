e503cac578b885d78c56efdabc78599823a0274a
pranav02082003 <luckypranav47@gmail.com>
2023-09-22 21:37:53 +0530

    admin panel start

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -3,9 +3,17 @@ import "./Login.css"
 import { FormControl, IconButton, Input, InputAdornment, InputLabel, TextField } from '@mui/material'
 import Visibility from '@mui/icons-material/Visibility';
 import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import axios from 'axios'
+import Cookies from 'js-cookie'
+import {useNavigate} from 'react-router-dom'
 
 export default function Login() {
     const [showPassword, setShowPassword] = useState(false);
+    const [username,setUsername] = useState("")
+    const [password,setPassword]  =useState("")
+    const [error,setError] = useState("")
+
+    const navigate = useNavigate()
 
     const handleClickShowPassword = () => setShowPassword((show) => !show);
 
@@ -15,19 +23,38 @@ export default function Login() {
 
     const handleSubmit = (e) => {
         e.preventDefault()
+        const data = {
+            username,
+            password
+        }
+        axios.post("http://localhost:4000/login",data).then((res) => {
+            console.log(res)
+            if (res.data.token) {
+                const {token} = res.data
+                Cookies.set("jwt_token",token,1)
+                navigate('/',{replace:true})
+            }
+        }).catch((err) => {
+            console.log(err)
+            if (err.response.status === 404) {
+                setError(err.response.data)
+            }
+        })
     }
 
     return (
         <div className='Login-edit'>
             <form className='login-form' onSubmit={handleSubmit}>
                 <h1>Admin Login</h1>
-                <TextField type='text' id="Username" label="Username" variant="standard" required />
+                <TextField type='text' id="Username" label="Username" variant="standard" value={username} required onChange={(e) => setUsername(e.target.value)} />
                 <FormControl variant="standard">
                     <InputLabel htmlFor="standard-adornment-password">Password</InputLabel>
                     <Input
                         id="standard-adornment-password"
                         type={showPassword ? 'text' : 'password'}
                         required
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
                         endAdornment={
                             <InputAdornment position="end">
                                 <IconButton
@@ -41,7 +68,8 @@ export default function Login() {
                         }
                     />
                 </FormControl>
-                <button type="submit" class="btn btn-outline-success">Login</button>
+                <div>{error.length > 0 && <span>{error}</span>}</div>
+                <button type="submit" className="btn btn-outline-success">Login</button>
             </form>
         </div>
 
